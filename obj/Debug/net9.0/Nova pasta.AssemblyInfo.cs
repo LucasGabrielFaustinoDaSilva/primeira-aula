a597f96ec512413cf7ca5e94c3bc4c85715b3d4e
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-08-04 20:33:02 -0300

    Atualizado

--- obj/Debug/net9.0/Nova pasta.AssemblyInfo.cs
+++ obj/Debug/net9.0/Nova pasta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nova pasta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bea1c88b722931dd5a8451fe6e4fc6b60d9bd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nova pasta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nova pasta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
